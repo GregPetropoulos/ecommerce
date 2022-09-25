d0fdbed9b5a6486adaed248d57e37e665df3eb71
GP <gregpetropoulos@yahoo.com>
2022-09-24 21:33:24 -0400

    Added search component to nav bar and it's mobile responsive.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,204 +1,317 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
+
 const Navbar = () => {
+  const [isSearch, setIsSearch] = useState(false);
+
   return (
-    <div className='navbar bg-base-100'>
-      <div className='navbar-start'>
-        <div className='dropdown'>
-          <label tabIndex={0} className='btn btn-ghost lg:hidden'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'>
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h8m-8 6h16'
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
-            <li>
-              <Link to='/'>Item 1</Link>
-            </li>
-            <li tabIndex={0}>
-              <Link  to='/' className='justify-between'>
+    <>
+      <div className='form-control m-3 sm:hidden'>
+          <div className='flex justify-center '>
+            <input
+              type='text'
+              placeholder='Search'
+              className='input input-xs input-bordered'
+            />
+          </div>
+      </div>
+      <div className='navbar bg-base-100'>
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h8m-8 6h16'
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
+ <li tabIndex={0}>
+              <Link to='/shopall'>
                 Shop All
                 <svg
                   className='fill-current'
                   xmlns='http://www.w3.org/2000/svg'
-                  width='24'
-                  height='24'
+                  width='20'
+                  height='20'
                   viewBox='0 0 24 24'>
-                  <path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' />
+                  <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
                 </svg>
               </Link>
-              <ul className='p-2'></ul>
+              <ul className='p-2'>
+                <li>
+                  <Link to='/feature'>Featured</Link>
+                </li>
+                <li>
+                  <Link to='/newarrivals'>New Arrivals</Link>
+                </li>
+                <li>
+                  <Link to='/freeexclusive'>Package Free Exclusives</Link>
+                </li>
+                <li>
+                  <Link to='/'>Household</Link>
+                </li>
+                <li>
+                  <Link to='/beautybody'>Beauty & Body</Link>
+                </li>
+                <li>
+                  <Link to='/family'>Family & Kids</Link>
+                </li>
+                <li>
+                  <Link to='/pet'>Pet</Link>
+                </li>
+                <li>
+                  <Link to='/clothing'>Clothing & Accessories</Link>
+                </li>
+                <li>
+                  <Link to='/bundlesave'>Bundle & Save</Link>
+                </li>
+                <li>
+                  <Link to='/bybrand'>Shop By Brand</Link>
+                </li>
+                <li>
+                  <Link to='/clearance'>CLEARANCE</Link>
+                </li>
+                <li>
+                  <Link to='/shopall'>Shop All</Link>
+                </li>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+              </ul>
+            </li>
+            <li tabIndex={0}>
+              <Link to='/bundlesave'>
+                Bundle & Save
+                <svg
+                  className='fill-current'
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='20'
+                  height='20'
+                  viewBox='0 0 24 24'>
+                  <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
+                </svg>
+              </Link>
+              <ul className='p-2'>
+                <li>
+                  <Link to='/aboutus'>About Us</Link>
+                </li>
+                <li>
+                  <Link to='/ourimpact'>Our Impact</Link>
+                </li>
+                <li>
+                  <Link to='/principles'>Our Guiding Principles</Link>
+                </li>
+                <li>
+                  <Link to='/howweship'>How We Ship</Link>
+                </li>
+                <li>
+                  <Link to='/livemore'>Steps To Start Living More</Link>
+                </li>
+                <li>
+                  <Link to='/sustainably'>Sustainably</Link>
+                </li>
+                <li>
+                  <Link to='/contactus'>Contact Us</Link>
+                </li>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+              </ul>
             </li>
             <li>
-              <Link to='/'>Item 3</Link>
+              <Link to='/aboutus'>About Us</Link>
+            </li>
+
+            <li>
+              <Link to='/rewards'>Rewards</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            </ul>
+          </div>
+          <Link
+            to='/'
+            className='btn btn-xs btn-ghost normal-case text-sm  sm:text-xl'>
+            HouseGoods
+          </Link>
+        </div>
+        <div className='navbar-center hidden md:flex'>
+          <ul className='menu menu-horizontal p-0'>
+            <li tabIndex={0}>
+              <Link to='/shopall'>
+                Shop All
+                <svg
+                  className='fill-current'
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='20'
+                  height='20'
+                  viewBox='0 0 24 24'>
+                  <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
+                </svg>
+              </Link>
+              <ul className='p-2'>
+                <li>
+                  <Link to='/feature'>Featured</Link>
+                </li>
+                <li>
+                  <Link to='/newarrivals'>New Arrivals</Link>
+                </li>
+                <li>
+                  <Link to='/freeexclusive'>Package Free Exclusives</Link>
+                </li>
+                <li>
+                  <Link to='/'>Household</Link>
+                </li>
+                <li>
+                  <Link to='/beautybody'>Beauty & Body</Link>
+                </li>
+                <li>
+                  <Link to='/family'>Family & Kids</Link>
+                </li>
+                <li>
+                  <Link to='/pet'>Pet</Link>
+                </li>
+                <li>
+                  <Link to='/clothing'>Clothing & Accessories</Link>
+                </li>
+                <li>
+                  <Link to='/bundlesave'>Bundle & Save</Link>
+                </li>
+                <li>
+                  <Link to='/bybrand'>Shop By Brand</Link>
+                </li>
+                <li>
+                  <Link to='/clearance'>CLEARANCE</Link>
+                </li>
+                <li>
+                  <Link to='/shopall'>Shop All</Link>
+                </li>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+              </ul>
+            </li>
+            <li tabIndex={0}>
+              <Link to='/bundlesave'>
+                Bundle & Save
+                <svg
+                  className='fill-current'
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='20'
+                  height='20'
+                  viewBox='0 0 24 24'>
+                  <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
+                </svg>
+              </Link>
+              <ul className='p-2'>
+                <li>
+                  <Link to='/aboutus'>About Us</Link>
+                </li>
+                <li>
+                  <Link to='/ourimpact'>Our Impact</Link>
+                </li>
+                <li>
+                  <Link to='/principles'>Our Guiding Principles</Link>
+                </li>
+                <li>
+                  <Link to='/howweship'>How We Ship</Link>
+                </li>
+                <li>
+                  <Link to='/livemore'>Steps To Start Living More</Link>
+                </li>
+                <li>
+                  <Link to='/sustainably'>Sustainably</Link>
+                </li>
+                <li>
+                  <Link to='/contactus'>Contact Us</Link>
+                </li>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to='/aboutus'>About Us</Link>
+            </li>
+
+            <li>
+              <Link to='/rewards'>Rewards</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
             </li>
           </ul>
         </div>
-        <Link  to='/' className='btn btn-ghost normal-case text-xl'>HouseGoods</Link>
-      </div>
-      <div className='navbar-center hidden lg:flex'>
-        <ul className='menu menu-horizontal p-0'>
-          <li tabIndex={0}>
-            <Link to='/shopall'>
-              Shop All
-              <svg
-                className='fill-current'
-                xmlns='http://www.w3.org/2000/svg'
-                width='20'
-                height='20'
-                viewBox='0 0 24 24'>
-                <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
-              </svg>
-            </Link>
-            <ul className='p-2'>
-              <li>
-                <Link to='/feature'>Featured</Link>
-              </li>
-              <li>
-                <Link to='/newarrivals'>New Arrivals</Link>
-              </li>
-              <li>
-                <Link to='/freeexclusive'>Package Free Exclusives</Link>
-              </li>
-              <li>
-                <Link to='/'>Household</Link>
-              </li>
-              <li>
-                <Link to='/beautybody'>Beauty & Body</Link>
-              </li>
-              <li>
-                <Link to='/family'>Family & Kids</Link>
-              </li>
-              <li>
-                <Link to='/pet'>Pet</Link>
-              </li>
-              <li>
-                <Link to='/clothing'>Clothing & Accessories</Link>
-              </li>
-              <li>
-                <Link to='/bundlesave'>Bundle & Save</Link>
-              </li>
-              <li>
-                <Link to='/bybrand'>Shop By Brand</Link>
-              </li>
-              <li>
-                <Link to='/clearance'>CLEARANCE</Link>
-              </li>
-              <li>
-                <Link to='/shopall'>Shop All</Link>
-              </li>
-              <li>
-                <Link to='/login'>Login</Link>
-              </li>
-            </ul>
-          </li>
-          <li tabIndex={0}>
-            <Link to='/bundlesave'>
-              Bundle & Save
-              <svg
-                className='fill-current'
-                xmlns='http://www.w3.org/2000/svg'
-                width='20'
-                height='20'
-                viewBox='0 0 24 24'>
-                <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
-              </svg>
-            </Link>
-            <ul className='p-2'>
-              <li>
-                <Link to='/aboutus'>About Us</Link>
-              </li>
-              <li>
-                <Link to='/ourimpact'>Our Impact</Link>
-              </li>
-              <li>
-                <Link to='/principles'>Our Guiding Principles</Link>
-              </li>
-              <li>
-                <Link to='/howweship'>How We Ship</Link>
-              </li>
-              <li>
-                <Link to='/livemore'>Steps To Start Living More</Link>
-              </li>
-              <li>
-                <Link to='/sustainably'>Sustainably</Link>
-              </li>
-              <li>
-                <Link to='/contactus'>Contact Us</Link>
-              </li>
-              <li>
-                <Link to='/login'>Login</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to='/aboutus'>About Us</Link>
-          </li>
-
-          <li>
-            <Link to='/rewards'>Rewards</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-        </ul>
-      </div>
-      <div className='navbar-end'>
-        <div className='flex flex-row'>
-          <div className='form-control'>
-            <input
-              type='text'
-              placeholder='Search'
-              className='input input-bordered'
-            />
-          </div>
-          <div className='dropdown dropdown-end'>
-            <label tabIndex={0} className='btn btn-ghost btn-circle'>
-              <div className='indicator'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='h-5 w-5'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'>
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+        <div className='navbar-end'>
+          <div className='flex flex-row justify-center'>
+            <div className='hidden sm:form-control sm:mt-3'>
+              {isSearch ? (
+                <div className='flex justify-center z-10'>
+                  <input
+                    type='text'
+                    placeholder='Search'
+                    className='input input-xs input-bordered'
                   />
-                </svg>
-                <span className='badge badge-sm indicator-item'>8</span>
-              </div>
-            </label>
-            <div
-              tabIndex={0}
-              className='mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow'>
-              <div className='card-body'>
-                <span className='font-bold text-lg'>8 Items</span>
-                <span className='text-info'>Subtotal: $999</span>
-                <div className='card-actions'>
-                  <button className='btn btn-primary btn-block'>
-                    View cart
+                  <button
+                    className='btn btn-xs btn-square mx-1'
+                    onClick={() => setIsSearch(!isSearch)}>
+                    X
                   </button>
+                </div>
+              ) : (
+                <FiSearch onClick={() => setIsSearch(!isSearch)} />
+              )}
+            </div>
+            <div className='dropdown dropdown-end'>
+              <label tabIndex={0} className='btn btn-ghost btn-circle'>
+                <div className='indicator'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-5 w-5'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'>
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                    />
+                  </svg>
+                  <span className='badge badge-sm indicator-item'>8</span>
+                </div>
+              </label>
+              <div
+                tabIndex={0}
+                className='mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow'>
+                <div className='card-body'>
+                  <span className='font-bold text-lg'>8 Items</span>
+                  <span className='text-info'>Subtotal: $999</span>
+                  <div className='card-actions'>
+                    <button className='btn btn-primary btn-block'>
+                      View cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
